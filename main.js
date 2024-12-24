5890778beb7dd4a1f30d6eb89746685b65910c02
Raulsv10 <50533364+Raulsv10@users.noreply.github.com>
2024-12-23 20:35:16 -0600

    ajuste para imágenes y liga js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const Producto = function (id, nombre, dsc, precio) {
+const Producto = function (id, nombre, dsc, precio, imgN) {
   this.id = id;
   this.nombre = nombre;
   this.dsc = dsc;
   this.precio = precio;
+  this.imgN = imgN;
 };
 
 const carrito = function (id, nombre, cantidad, precio) {
@@ -16,49 +17,57 @@ let p1 = new Producto(
   1,
   "Café Americano",
   "El es una bebida de café preparada con agua caliente y un espresso, lo que resulta en un sabor más suave y menos intenso que el espresso puro, pero con una textura ligera y aromática.",
-  60
+  60,
+  "Americano.jpeg"
 );
 let p2 = new Producto(
   2,
   "Cappuccino",
   "El cappuccino es una bebida de café italiana que mezcla espresso, leche caliente y espuma de leche en partes iguales, ofreciendo un sabor equilibrado y una textura cremosa.",
-  68
+  68,
+  "Cappucino.jpeg"
 );
 let p3 = new Producto(
   3,
   "Café Espresso",
   "El café espresso es una bebida concentrada de sabor intenso, preparada al pasar agua caliente a presión por café molido fino, con una capa de crema en la superficie.",
-  73
+  73,
+  "Esppresso.jpeg"
 );
 let p4 = new Producto(
   4,
   "Café Latte",
   "El café latte es una bebida suave que combina un shot de espresso con abundante leche caliente y una ligera capa de espuma, ofreciendo un sabor cremoso y equilibrado.",
-  65
+  65,
+  "Latte.jpeg"
 );
 let p5 = new Producto(
   5,
   "Té Verde",
   "El té verde es una infusión natural hecha de hojas no oxidadas de la planta Camellia sinensis, con un sabor suave y fresco, rico en antioxidantes y propiedades saludables.",
-  53
+  53,
+  "Verde.jpeg"
 );
 let p6 = new Producto(
   6,
   "Té Chai",
   "El té chai es una mezcla aromática de té negro con especias como canela, cardamomo, jengibre y clavo, combinado con leche y azúcar, ofreciendo un sabor cálido y especiado.",
-  56
+  56,
+  "Chai.jpeg"
 );
 let p7 = new Producto(
   7,
   "Mate",
   "Es una infusión tradicional sudamericana hecha con hojas de yerba mate, conocida por su sabor herbal y amargo, además de sus propiedades energizantes.",
-  80
+  80,
+  "Mate.jpeg"
 );
 let p8 = new Producto(
   8,
   "Frappe",
   "El frappé es una bebida fría y espumosa hecha a base de café, hielo y, a menudo, leche o crema, mezclados hasta obtener una textura cremosa y refrescante.",
-  120
+  120,
+  "Frappe.jpeg"
 );
 
 let lstProductos = [p1, p2, p3, p4, p5, p6, p7, p8];
@@ -71,7 +80,7 @@ let item = "";
 lstProductos.forEach((element) => {
   //console.log(element.nombre);
   item += `<div class="card menu shadow-sm p-2 m-2" style="width: 24rem;">
-            <img src="https://via.placeholder.com/300x200" alt="${element.nombre}">
+            <img src="./asset/img/${element.imgN}" alt="${element.nombre}">
             <div class="content">
                 <h3>${element.nombre}</h3>
                 <p class="card-body">${element.dsc}</p>
